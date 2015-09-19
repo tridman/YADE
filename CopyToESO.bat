d45fb0f2c02b54c58ed206fdf6641a5750dc1da0
tridman <tridman@web.de>
2015-09-19 16:17:04 +0200

    - Updated API-Version to 100012 - Updated LibAddonMenu to 2.0r18

--- CopyToESO.bat
+++ CopyToESO.bat
@@ -1,9 +1,9 @@
 echo off
 
 echo "Removing old directory..."
-rmdir /S /Q "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\YADE\"
+rmdir /S /Q "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\YADE\"
 echo "DONE!"
 
 echo "Coppy new sources...."
-xcopy YADE "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\YADE" /S /E /C /I
+xcopy YADE "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\YADE" /S /E /C /I
 echo "DONE!"
