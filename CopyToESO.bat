72e6038597b69cd25d7976b86e015e0ad7241dd8
tridman <tridman@web.de>
2016-11-23 20:49:17 +0100

    API Update

--- CopyToESO.bat
+++ CopyToESO.bat
@@ -1,9 +1,9 @@
 echo off
 
 echo "Removing old directory..."
-rmdir /S /Q "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\YADE\"
+rmdir /S /Q "F:\home\tridman\Documents\Elder Scrolls Online\live\AddOns\YADE\"
 echo "DONE!"
 
 echo "Coppy new sources...."
-xcopy YADE "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\YADE" /S /E /C /I
+xcopy YADE "F:\home\tridman\Documents\Elder Scrolls Online\live\AddOns\YADE" /S /E /C /I
 echo "DONE!"
